7cfb4c472d40e922389a3d92c6a7064a94dc6cfd
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-19 21:06:13 +0100

    dummy data

--- src/store/models/mainModel.ts
+++ src/store/models/mainModel.ts
@@ -1,7 +1,29 @@
+import { Action, action } from "easy-peasy";
+import { LineItem } from "../../types";
+
 export interface MainModel {
-	message: string;
+	// state
+	lineItems: LineItem[];
+
+	// actions
+	loadSiteData: Action<this>;
 }
 
 export const mainModel: MainModel = {
-	message: "Welcome to this site, more coming soon...",
+	// state
+	lineItems: [],
+
+	// actions
+	loadSiteData: action((state) => {
+		state.lineItems.push({
+			chapter: 1,
+			lineNumber: 1,
+			rawText: "nnn",
+		});
+		state.lineItems.push({
+			chapter: 2,
+			lineNumber: 1,
+			rawText: "nnn",
+		});
+	}),
 };
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type LineItem = {
+	chapter: number;
+	lineNumber: number;
+	rawText: string;
+}
