918b098e1aa6ceecf79784ce285c2b08317f9545
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-20 00:00:41 +0100

    chapter ok

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useTypedStoreState } from '../store/hooks';
 
 export const PageWelcome = () => {
@@ -5,27 +6,26 @@ export const PageWelcome = () => {
 
 	return (
 		<>
-			<ul className='list-disc ml-6'>
-				{smartBook.chapters.map((chapter, index) => {
-					return (
-						<li key={index}>CHAPTER {chapter.number}
-							<ul>
-								{chapter.smartLines.map((smartLine, index) => {
-									return (
-										<li key={index} >{smartLine.number}
-											<ul>
-												<li>{smartLine.rawTexts.fr}</li>
-												<li>{smartLine.rawTexts.sp}</li>
-												<li>{smartLine.rawTexts.it}</li>
-											</ul>
-										</li>
-									)
-								})}
-							</ul>
-						</li>
-					)
-				})}
-			</ul>
+			{smartBook.chapters.map((chapter, index) => {
+				return (
+					<React.Fragment>
+						<h2 className='text-2xl'>CHAPTER {chapter.number}</h2>
+						<ul>
+							{chapter.smartLines.map((smartLine, index) => {
+								return (
+									<li key={index} >{smartLine.number}
+										<ul>
+											<li>{smartLine.rawTexts.fr}</li>
+											<li>{smartLine.rawTexts.sp}</li>
+											<li>{smartLine.rawTexts.it}</li>
+										</ul>
+									</li>
+								)
+							})}
+						</ul>
+					</React.Fragment>
+				)
+			})}
 		</>
 	)
 }
