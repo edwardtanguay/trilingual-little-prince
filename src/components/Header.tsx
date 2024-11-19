c34680f8e9b5bb128ab75fb3d419b119f3c0d227
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-20 00:49:33 +0100

    repo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
 	return (
 		<>
 			<h1 className="text-[1.8rem] text-slate-800">Trilingual Little Prince</h1>
-			<h2 className="mb-3 text-sm italic">made with React, TypeScript, Tailwind, Easy-Peasy Redux, see <a href="https://github.com/edwardtanguay/trilingual-little-prince" className="underline">repo</a>, <a href="https://trilingual-little-prince.vercel.app" className="underline">site</a> or more of my <a className="underline" href="https://tanguay-eu.vercel.app/howtos">projects</a></h2>
+			<h2 className="mb-3 text-sm italic">made with React, TypeScript, Tailwind, Easy-Peasy Redux, see <a target="_blank" href="https://github.com/edwardtanguay/trilingual-little-prince" className="underline">repo</a>, <a href="https://trilingual-little-prince.vercel.app" target="_blank" className="underline">site</a> or more of my <a className="underline" target="_blank" href="https://tanguay-eu.vercel.app/howtos">projects</a></h2>
 			<Nav />
 		</>
 	);
