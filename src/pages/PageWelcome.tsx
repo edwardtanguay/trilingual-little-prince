6e4e9181ee5b465ff701ae99f588bd75a98bd127
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-20 00:04:12 +0100

    three showing with number

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -8,21 +8,20 @@ export const PageWelcome = () => {
 		<>
 			{smartBook.chapters.map((chapter, index) => {
 				return (
-					<React.Fragment>
-						<h2 className='text-2xl'>CHAPTER {chapter.number}</h2>
-						<ul>
-							{chapter.smartLines.map((smartLine, index) => {
-								return (
-									<li key={index} >{smartLine.number}
-										<ul>
-											<li>{smartLine.rawTexts.fr}</li>
-											<li>{smartLine.rawTexts.sp}</li>
-											<li>{smartLine.rawTexts.it}</li>
-										</ul>
-									</li>
-								)
-							})}
-						</ul>
+					<React.Fragment key={index}>
+						<h2 className='text-2xl mb-3'>CHAPTER {chapter.number}</h2>
+						{chapter.smartLines.map((smartLine, index) => {
+							return (
+								<div key={index} className='flex gap-3 mb-3'>
+									<div>{smartLine.number}</div>
+									<div>
+										<div>{smartLine.rawTexts.fr}</div>
+										<div>{smartLine.rawTexts.sp}</div>
+										<div>{smartLine.rawTexts.it}</div>
+									</div>
+								</div>
+							)
+						})}
 					</React.Fragment>
 				)
 			})}
