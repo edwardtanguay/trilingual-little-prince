6df1b6ea1bd5aa58202785f920e5be0bcbca1878
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-20 00:37:57 +0100

    note

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-[1.8rem] mb-3 text-slate-800">Trilingual Little Prince</h1>
-			<Nav/>
+			<h1 className="text-[1.8rem] text-slate-800">Trilingual Little Prince</h1>
+			<h2 className="mb-3 text-sm italic">made with React, TypeScript, Tailwind, Easy-Peasy Redux, see <a href="https://github.com/edwardtanguay/trilingual-little-prince" className="underline">repo</a>, <a href="https://trilingual-little-prince.vercel.app" className="underline">site</a> or more of my <a className="underline" href="https://tanguay-eu.vercel.app/howtos">projects</a></h2>
+			<Nav />
 		</>
 	);
 };
--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,8 @@
 export const PageAbout = () => {
 	return (
-		<p>My tandem partner and I are reading the Little Prince in French, Spanish and Italian. This site shows my interactive linguistic notes based on the text.</p>
+		<>
+			<p className="mb-3">My language tandem partner and I are reading the Little Prince in French, Spanish and Italian. The goal of this site is to enable me to take interactive linguistic notes based on the texts.</p>
+			<p>This site is also a way to experiment with <a className="underline" href="https://easy-peasy.vercel.app" target="_blank">easy-peasy Redux</a> in a realistic site, a state management solution for React I can recommend.</p>
+		</>
 	)
 }
