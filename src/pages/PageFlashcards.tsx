ba3a14e5c8d214d3bb140a1ed59c07b34125b180
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-22 00:01:01 +0100

    show top

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -8,8 +8,10 @@ export const PageFlashcards = () => {
 			{flashcards.map((flashcard) => {
 				return (
 					<div key={flashcard.idCode} className="mb-3 w-fit font-mono">
-						<p className=" bg-slate-500 pt-1 pb-1 px-3 rounded-t-md">{flashcard.front}</p>
-						<p className=" bg-slate-600 text-yellow-200 pt-1 pb-2 px-3 rounded-b-md ">{flashcard.back}</p>
+						<p className="text-slate-200 bg-slate-500 pt-1 pb-1 px-3 rounded-t-md">{flashcard.front}</p>
+						{flashcard.isShowing && (
+							<p className=" bg-slate-600 text-yellow-200 pt-1 pb-2 px-3 rounded-b-md ">{flashcard.back}</p>
+						)}
 					</div>
 				)
 			})}
