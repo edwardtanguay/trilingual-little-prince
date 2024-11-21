4fdef4163fce6cfae333d52930e77f77d0ee1edb
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-21 22:58:39 +0100

    flashcards loading with nnn

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -8,8 +8,11 @@ export const PageFlashcards = () => {
 			{flashcards.map((flashcard, index) => {
 				return (
 					<div key={index} className="mb-3">
+						<p>{flashcard.idCode}</p>
 						<p>{flashcard.front}</p>
 						<p>{flashcard.back}</p>
+						<p>{flashcard.bulkSearch}</p>
+						<p>{flashcard.isShowing ? 'showing' : 'no showing'}</p>
 					</div>
 				)
 			})}
