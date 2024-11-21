e8a746b73253a70635d5b25ba5f5db66df7c5c9c
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-21 23:59:27 +0100

    showing top/bottom

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -7,12 +7,9 @@ export const PageFlashcards = () => {
 		<>
 			{flashcards.map((flashcard) => {
 				return (
-					<div key={flashcard.idCode} className="mb-3">
-						<p>{flashcard.idCode}</p>
-						<p>{flashcard.front}</p>
-						<p>{flashcard.back}</p>
-						<p>[{flashcard.bulkSearch}]</p>
-						<p>{flashcard.isShowing ? 'showing' : 'no showing'}</p>
+					<div key={flashcard.idCode} className="mb-3 w-fit font-mono">
+						<p className=" bg-slate-500 pt-1 pb-1 px-3 rounded-t-md">{flashcard.front}</p>
+						<p className=" bg-slate-600 text-yellow-200 pt-1 pb-2 px-3 rounded-b-md ">{flashcard.back}</p>
 					</div>
 				)
 			})}
