f96dbdaba5627d086b6fa4b9712e61534b1534ed
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-24 18:59:51 +0100

    showing first

--- src/components/FlashcardTraining.tsx
+++ src/components/FlashcardTraining.tsx
@@ -1,3 +1,15 @@
+import { useTypedStoreState } from "../store/easy-peasy-hooks";
+
 export const FlashcardTraining = () => {
-	return <p>flashcard training</p>;
+	const { flashcards } = useTypedStoreState((state) => state.flashcardModel);
+
+	const currentFlashcard = flashcards[0];
+	return (
+		<div className="bg-gray-300 mb-6 p-3 w-fit rounded">
+			<p className="mb-3">{currentFlashcard.front}</p>
+			<div className="flex gap-3">
+			<input className="rounded w-[30rem]" placeholder="spanish"/><button className="bg-gray-400 text-sm py-0 px-2 rounded">submit</button>
+			</div>
+		</div>
+	);
 };
