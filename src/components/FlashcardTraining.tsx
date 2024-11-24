36634c85d09efa03db00d850999804d752cb4d2c
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-24 18:49:27 +0100

    base component

---
+++ src/components/FlashcardTraining.tsx
@@ -0,0 +1,3 @@
+export const FlashcardTraining = () => {
+	return <p>flashcard training</p>;
+};
--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -1,25 +1,57 @@
-import { useTypedStoreActions, useTypedStoreState } from "../store/easy-peasy-hooks";
+import { FlashcardTraining } from "../components/FlashcardTraining";
+import {
+	useTypedStoreActions,
+	useTypedStoreState,
+} from "../store/easy-peasy-hooks";
 
 export const PageFlashcards = () => {
-	const { filteredFlashcards, flashcardsSearchText, flashcardNumberShowingMessage } = useTypedStoreState(state => state.flashcardModel);
-	const { toggleFlashcard, handleFlashcardSearchTextChange } = useTypedStoreActions(actions => actions.flashcardModel);
+	const {
+		filteredFlashcards,
+		flashcardsSearchText,
+		flashcardNumberShowingMessage,
+	} = useTypedStoreState((state) => state.flashcardModel);
+	const { toggleFlashcard, handleFlashcardSearchTextChange } =
+		useTypedStoreActions((actions) => actions.flashcardModel);
 
 	return (
 		<>
+			<FlashcardTraining />
 			<form>
-				<input type="text" value={flashcardsSearchText} onChange={(e) => handleFlashcardSearchTextChange(e.target.value)} className="w-[10rem] mb-3 text-2xl" />
+				<input
+					type="text"
+					value={flashcardsSearchText}
+					onChange={(e) =>
+						handleFlashcardSearchTextChange(e.target.value)
+					}
+					className="w-[10rem] mb-3 text-2xl"
+				/>
 			</form>
-			<p className="mb-3" dangerouslySetInnerHTML={{ __html: flashcardNumberShowingMessage }}></p>
+			<p
+				className="mb-3"
+				dangerouslySetInnerHTML={{
+					__html: flashcardNumberShowingMessage,
+				}}
+			></p>
 			{filteredFlashcards.map((filteredFlashcard) => {
 				return (
-					<div key={filteredFlashcard.idCode} className="mb-3 w-fit font-mono">
-						<p onClick={() => toggleFlashcard(filteredFlashcard)} className="text-slate-900 bg-slate-500 pt-1 pb-1 px-3 rounded-t-md select-none cursor-pointer">{filteredFlashcard.front}</p>
+					<div
+						key={filteredFlashcard.idCode}
+						className="mb-3 w-fit font-mono"
+					>
+						<p
+							onClick={() => toggleFlashcard(filteredFlashcard)}
+							className="text-slate-900 bg-slate-500 pt-1 pb-1 px-3 rounded-t-md select-none cursor-pointer"
+						>
+							{filteredFlashcard.front}
+						</p>
 						{filteredFlashcard.isShowing && (
-							<p className=" bg-slate-600 text-yellow-200 pt-1 pb-2 px-3 rounded-b-md ">{filteredFlashcard.back}</p>
+							<p className=" bg-slate-600 text-yellow-200 pt-1 pb-2 px-3 rounded-b-md ">
+								{filteredFlashcard.back}
+							</p>
 						)}
 					</div>
-				)
+				);
 			})}
 		</>
-	)
-}
+	);
+};
