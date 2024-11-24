1a7f6ef0d04af56fe7bb10bb8e8864a0aff098f1
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-24 19:01:28 +0100

    button hover

--- src/components/FlashcardTraining.tsx
+++ src/components/FlashcardTraining.tsx
@@ -3,12 +3,12 @@ import { useTypedStoreState } from "../store/easy-peasy-hooks";
 export const FlashcardTraining = () => {
 	const { flashcards } = useTypedStoreState((state) => state.flashcardModel);
 
-	const currentFlashcard = flashcards[0];
+	const currentFlashcard = flashcards[1];
 	return (
 		<div className="bg-gray-300 mb-6 p-3 w-fit rounded">
 			<p className="mb-3">{currentFlashcard.front}</p>
 			<div className="flex gap-3">
-			<input className="rounded w-[30rem]" placeholder="spanish"/><button className="bg-gray-400 text-sm py-0 px-2 rounded">submit</button>
+			<input className="rounded w-[30rem]" placeholder="spanish"/><button className="bg-gray-400 opacity-80 text-sm py-0 px-2 rounded hover:opacity-100">submit</button>
 			</div>
 		</div>
 	);
