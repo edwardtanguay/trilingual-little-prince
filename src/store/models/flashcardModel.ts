81fc822db2fa5339994ec567d31a034485c53de0
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-25 14:26:52 +0100

    working

--- src/store/models/flashcardModel.ts
+++ src/store/models/flashcardModel.ts
@@ -38,93 +38,94 @@ export interface FlashcardModel {
 	setNumberRight: Action<this, number>;
 	setNumberWrong: Action<this, number>;
 	addWrongAnswer: Action<this, string>;
-	// getFlashcardHistoryItem: Action<this, string>;
 }
 
-export const flashcardModel: FlashcardModel = persist({
-	// state
-	flashcards: [],
-	flashcardsSearchText: "",
-	testingFlashcard: emptyFlashcard,
-	answer: "",
-	answerIsCorrect: false,
-	testingStatus: "typingAnswer",
-	numberRight: 0,
-	numberWrong: 0,
-	wrongAnswers: [],
-	user: blankUser,
+export const flashcardModel: FlashcardModel = persist(
+	{
+		// state
+		flashcards: [],
+		flashcardsSearchText: "",
+		testingFlashcard: emptyFlashcard,
+		answer: "",
+		answerIsCorrect: false,
+		testingStatus: "typingAnswer",
+		numberRight: 0,
+		numberWrong: 0,
+		wrongAnswers: [],
+		user: blankUser,
 
-	// computed state
-	filteredFlashcards: computed((state) => {
-		if (state.flashcardsSearchText.trim() === "") {
-			return state.flashcards;
-		} else {
-			return state.flashcards.filter((m) =>
-				m.bulkSearch.includes(state.flashcardsSearchText)
+		// computed state
+		filteredFlashcards: computed((state) => {
+			if (state.flashcardsSearchText.trim() === "") {
+				return state.flashcards;
+			} else {
+				return state.flashcards.filter((m) =>
+					m.bulkSearch.includes(state.flashcardsSearchText)
+				);
+			}
+		}),
+		flashcardNumberShowingMessage: computed((state) => {
+			if (state.filteredFlashcards.length === state.flashcards.length) {
+				return `All <span class="font-bold">${state.flashcards.length}</span> flashcards are showing:`;
+			} else {
+				const verb =
+					state.filteredFlashcards.length === 1 ? "is" : "are";
+				return `There ${verb} <span class="font-bold">${state.filteredFlashcards.length}</span> of ${state.flashcards.length} flashcards showing:`;
+			}
+		}),
+		userFullName: computed((state) => {
+			return state.user.firstName + " " + state.user.lastName;
+		}),
+
+		// actions
+		loadFlashcards: action((state) => {
+			state.flashcards = dataModel.getFlashcards();
+			state.filteredFlashcards = structuredClone(state.flashcards);
+		}),
+		toggleFlashcard: action((state, flashcard) => {
+			const _flashcard = state.flashcards.find(
+				(m) => m.idCode === flashcard.idCode
 			);
-		}
-	}),
-	flashcardNumberShowingMessage: computed((state) => {
-		if (state.filteredFlashcards.length === state.flashcards.length) {
-			return `All <span class="font-bold">${state.flashcards.length}</span> flashcards are showing:`;
-		} else {
-			const verb = state.filteredFlashcards.length === 1 ? "is" : "are";
-			return `There ${verb} <span class="font-bold">${state.filteredFlashcards.length}</span> of ${state.flashcards.length} flashcards showing:`;
-		}
-	}),
-	userFullName: computed(state => {
-		return state.user.firstName + ' ' + state.user.lastName;
-	}),
-
-	// actions
-	loadFlashcards: action((state) => {
-		state.flashcards = dataModel.getFlashcards();
-		state.filteredFlashcards = structuredClone(state.flashcards);
-	}),
-	toggleFlashcard: action((state, flashcard) => {
-		const _flashcard = state.flashcards.find(
-			(m) => m.idCode === flashcard.idCode
-		);
-		if (_flashcard) {
-			_flashcard.isShowing = !_flashcard.isShowing;
-		}
-	}),
-	handleFlashcardSearchTextChange: action((state, searchText) => {
-		state.flashcardsSearchText = searchText;
-		state.flashcards.forEach((m) => (m.isShowing = false));
-	}),
-	setNextTestingFlashcard: action((state) => {
-		const randomIndex = Math.floor(Math.random() * state.flashcards.length);
-		state.testingFlashcard = state.flashcards[randomIndex];
-		state.answer = "";
-		state.numberRight = 0;
-		state.numberWrong = 0;
-		state.testingStatus = "typingAnswer";
-		state.wrongAnswers = [];
-	}),
-	setAnswer: action((state, answer) => {
-		state.answer = answer;
-	}),
-	setAnswerIsCorrect: action((state, answerIsCorrect) => {
-		state.answerIsCorrect = answerIsCorrect;
-	}),
-	setTestingStatus: action((state, testingSTatus) => {
-		state.testingStatus = testingSTatus;
-	}),
-	setNumberRight: action((state, numberRight) => {
-		state.numberRight = numberRight;
-	}),
-	setNumberWrong: action((state, numberWrong) => {
-		state.numberWrong = numberWrong;
-	}),
-	addWrongAnswer: action((state, wrongAnswer) => {
-		state.wrongAnswers.push(wrongAnswer);
-	}),
-	// getFlashcardHistoryItem: action((state, flashcardIdCode) => {
-	// 	state.wrongAnswers.push(state.user.flashcardHistory[flashcardIdCode]);
-	// }),
-
-}, {
-	storage: 'localStorage',
-	allow: ['user']
-});
+			if (_flashcard) {
+				_flashcard.isShowing = !_flashcard.isShowing;
+			}
+		}),
+		handleFlashcardSearchTextChange: action((state, searchText) => {
+			state.flashcardsSearchText = searchText;
+			state.flashcards.forEach((m) => (m.isShowing = false));
+		}),
+		setNextTestingFlashcard: action((state) => {
+			const randomIndex = Math.floor(
+				Math.random() * state.flashcards.length
+			);
+			state.testingFlashcard = state.flashcards[randomIndex];
+			state.answer = "";
+			state.numberRight = 0;
+			state.numberWrong = 0;
+			state.testingStatus = "typingAnswer";
+			state.wrongAnswers = [];
+		}),
+		setAnswer: action((state, answer) => {
+			state.answer = answer;
+		}),
+		setAnswerIsCorrect: action((state, answerIsCorrect) => {
+			state.answerIsCorrect = answerIsCorrect;
+		}),
+		setTestingStatus: action((state, testingSTatus) => {
+			state.testingStatus = testingSTatus;
+		}),
+		setNumberRight: action((state, numberRight) => {
+			state.numberRight = numberRight;
+		}),
+		setNumberWrong: action((state, numberWrong) => {
+			state.numberWrong = numberWrong;
+		}),
+		addWrongAnswer: action((state, wrongAnswer) => {
+			state.wrongAnswers.push(wrongAnswer);
+		}),
+	},
+	{
+		storage: "localStorage",
+		allow: ["user"],
+	}
+);
