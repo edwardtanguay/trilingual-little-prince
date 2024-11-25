0a0d815a15aeef824ae565b36094f985898b8ac2
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-25 12:47:45 +0100

    name upper rigth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,41 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-[1.8rem] text-slate-800">Trilingual Little Prince</h1>
-			<h2 className="mb-3 text-sm italic">made with React, TypeScript, Tailwind, Easy-Peasy Redux, see <a target="_blank" href="https://github.com/edwardtanguay/trilingual-little-prince" className="underline">repo</a>, <a href="https://trilingual-little-prince.vercel.app" target="_blank" className="underline">site</a> or more of my <a className="underline" target="_blank" href="https://tanguay-eu.vercel.app/howtos">projects</a></h2>
+			<div className="flex justify-between">
+				<div>
+					<h1 className="text-[1.8rem] text-slate-800">
+						Trilingual Little Prince
+					</h1>
+					<h2 className="mb-3 text-sm italic">
+						made with React, TypeScript, Tailwind, Easy-Peasy Redux,
+						see{" "}
+						<a
+							target="_blank"
+							href="https://github.com/edwardtanguay/trilingual-little-prince"
+							className="underline"
+						>
+							repo
+						</a>
+						,{" "}
+						<a
+							href="https://trilingual-little-prince.vercel.app"
+							target="_blank"
+							className="underline"
+						>
+							site
+						</a>{" "}
+						or more of my{" "}
+						<a
+							className="underline"
+							target="_blank"
+							href="https://tanguay-eu.vercel.app/howtos"
+						>
+							projects
+						</a>
+					</h2>
+				</div>
+				<div>name</div>
+			</div>
 			<Nav />
 		</>
 	);
