817c6fc88637c68f6830569426032fb35c54f05e
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-11 23:54:54 +0100

    style for selected chapter

--- src/pages/PageText/ChapterNav.tsx
+++ src/pages/PageText/ChapterNav.tsx
@@ -4,6 +4,8 @@ import { useTypedStoreState } from "../../store/easy-peasy-hooks";
 export const ChapterNav = () => {
 	const { smartBook } = useTypedStoreState((state) => state.mainModel);
 
+	const currentChapterNumber = 2;
+
 	return (
 		<nav className="flex gap-3 bg-slate-500 px-4 py-2">
 			{smartBook.chapters.map((chapter) => {
@@ -12,7 +14,9 @@ export const ChapterNav = () => {
 						to={`/text/${chapter.number}`}
 						key={chapter.number}
 					>
-						<button className="btn-normal">{chapter.number}</button>
+						<button className={`btn-normal ${currentChapterNumber === chapter.number ? 'chapterSelected' : 'chapterUnselected'}`}>
+							{chapter.number}
+						</button>
 					</NavLink>
 				);
 			})}
