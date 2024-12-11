8ed2923ece0a5fe4bf48b786c032f2f4b9c7d83d
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-12 00:13:11 +0100

    finished, showing current chapter

--- src/pages/PageChapters/PageChapters.tsx
+++ src/pages/PageChapters/PageChapters.tsx
@@ -15,38 +15,40 @@ export const PageChapters = () => {
 		currentChapterNumber = 1;
 	}
 
+	const chapter = smartBook.chapters.find(
+		(m) => m.number === currentChapterNumber
+	);
+
 	return (
 		<>
-			<ChapterNav currentChapterNumber={currentChapterNumber} />
-			{smartBook.chapters.map((chapter, index) => {
-				return (
-					<React.Fragment key={index}>
-						<h2 className="text-2xl mb-3 mt-3">
-							CHAPTER {chapter.number}
-						</h2>
-						{chapter.smartLines.map((smartLine, index) => {
-							return (
-								<div key={index} className="flex gap-3 mb-3">
-									<div className="font-mono w-[.7rem] text-xs pt-[.3rem] opacity-50">
-										{smartLine.number}
+			{chapter && (
+				<>
+					<ChapterNav currentChapterNumber={currentChapterNumber} />
+					<h2 className="text-2xl mb-3 mt-3">
+						CHAPTER {chapter.number}
+					</h2>
+					{chapter.smartLines.map((smartLine, index) => {
+						return (
+							<div key={index} className="flex gap-3 mb-3">
+								<div className="font-mono w-[.7rem] text-xs pt-[.3rem] opacity-50">
+									{smartLine.number}
+								</div>
+								<div className="w-fit">
+									<div className="lang-fr">
+										{smartLine.plainTexts.fr}
 									</div>
-									<div className="w-fit">
-										<div className="lang-fr">
-											{smartLine.plainTexts.fr}
-										</div>
-										<div className="lang-sp">
-											{smartLine.plainTexts.sp}
-										</div>
-										<div className="lang-it">
-											{smartLine.plainTexts.it}
-										</div>
+									<div className="lang-sp">
+										{smartLine.plainTexts.sp}
+									</div>
+									<div className="lang-it">
+										{smartLine.plainTexts.it}
 									</div>
 								</div>
-							);
-						})}
-					</React.Fragment>
-				);
-			})}
+							</div>
+						);
+					})}
+				</>
+			)}
 		</>
 	);
 };
